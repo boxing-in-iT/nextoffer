d30f08d40c34ecb864e311d643e3331132f47e66
boxing-in-it <tbol240@gmail.com>
2025-02-02 20:34:06 +0200

    add simple switcher

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,13 +2,19 @@
 import { PageWrapper } from "@/components/PageWrapper/Login/PageWrapper";
 import { LoginForm } from "@/page-components/login/LoginForm";
 import Logo from "@/assets/logo.svg";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { usePathname } from "next/navigation";
 import { RegisterForm } from "@/page-components/register/RegisterForm";
 
 const LoginPage: FC = () => {
   const pathname = usePathname();
   const isLoginPage = pathname === "/login";
+  const [isLogin, setIsLogin] = useState(isLoginPage);
+
+  const handleSwitch = () => {
+    setIsLogin(!isLogin);
+  };
+
   return (
     <PageWrapper>
       <div className="mx-auto flex max-w-lg flex-col items-center justify-center">
@@ -25,11 +31,27 @@ const LoginPage: FC = () => {
           </p>
         </div>
 
-        <h2 className="mt-28 text-gray-dark text-[32px] font-semibold">
-          Welcome Back!
-        </h2>
-        {isLoginPage ? <LoginForm /> : <RegisterForm />}
-        {/* <LoginForm /> */}
+        {/* Switcher to change login and register */}
+        <div className="mt-4 flex justify-between">
+          <button
+            className={`text-lg font-semibold ${
+              isLogin ? "text-blue-500" : "text-gray-500"
+            }`}
+            onClick={handleSwitch}
+          >
+            Login
+          </button>
+          <button
+            className={`text-lg font-semibold ${
+              !isLogin ? "text-blue-500" : "text-gray-500"
+            }`}
+            onClick={handleSwitch}
+          >
+            Register
+          </button>
+        </div>
+
+        {isLogin ? <LoginForm /> : <RegisterForm />}
       </div>
     </PageWrapper>
   );
